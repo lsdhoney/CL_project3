d80e40642037b373f905362039ae523d98243887
Yiyang Cao <caoyiyang@msn.com>
2022-12-06 19:22:13 -0500

    2nd version

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 let express = require('express');
 let app = express();
 
-const { Database } = require("quickmongo");
-const db = new Database("mongodb+srv://yc:connect@cluster0.degfsdk.mongodb.net/?retryWrites=true&w=majority");
-
-db.on("ready", () => {
-    console.log("Connected to the database");
-    
-});
-
-db.connect(); 
-
 app.use(express.json());
 
-//let test = [];
+//DB
+let Datastore = require('nedb');
+let db = new Datastore('torturer.db');
+db.loadDatabase();
 
 app.post('/msgs',(req,res)=>{
     console.log(req.body);
@@ -22,21 +15,32 @@ app.post('/msgs',(req,res)=>{
     }
     //test.push(obj);
     //console.log(test);
-    db.push("msgData", obj);
-    res.json({task:"success"});
+    db.insert(obj,(err, newDocs)=>{
+        if(err) {
+            res.json({task: "task failed"});
+        } else {
+            res.json({task:"success"});
+        }
+
+    })
 })
 
 app.use(express.static('public'));
-let port = process.env.PORT || 3000;
-app.listen(port, ()=> {
-    console.log('listening at ', port);
-});
 
 app.get('/msgs',(req,res)=>{
-    db.get("msgData").then(mData =>{
-        let obj = {data:mData};
-        res.json(obj);
+    db.find({}, (err, docs)=> {
+        if(err) {
+            res.json({task: "task failed"})
+        } else {
+            let obj = {data: docs};
+            res.json(obj);
+        }
         
     });
     
 })
+
+let port = process.env.PORT || 3000;
+app.listen(port, ()=> {
+    console.log('listening at ', port);
+});
